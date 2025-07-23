3d5f1dba7e4714720f1f74784065a6dc220e32f7
Renato Miranda <renatusgod@gmail.com>
2025-07-23 09:39:41 -0300

    tirando o group name

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -934,14 +934,14 @@ export class WAStartupService {
         }
 
           // 👇 Aqui busca o nome do grupo (se for grupo)
-        if (isJidGroup(received.key.remoteJid)) {
-          try {
-            const metadata = await this.client.groupMetadata(received.key.remoteJid);
-            messageRaw['groupName'] = metadata?.subject;
-          } catch (err) {
-            this.logger.error(['Erro ao obter metadata do grupo', err?.message]);
-          }
-        }
+        // if (isJidGroup(received.key.remoteJid)) {
+        //   try {
+        //     const metadata = await this.client.groupMetadata(received.key.remoteJid);
+        //     messageRaw['groupName'] = metadata?.subject;
+        //   } catch (err) {
+        //     this.logger.error(['Erro ao obter metadata do grupo', err?.message]);
+        //   }
+        // }
         
         messageRaw['info'] = { type };
 
